12a66f7c71f7a2d1e3c618947ac4e2d87cf394c9
DoubleZ <kohei.tashiro@chiba-u.jp>
2020-07-31 15:22:17 +0900

    lambda&MySQL版のtweet-botを追加

---
+++ info_convert.py
@@ -0,0 +1,197 @@
+from enum import Enum,auto,unique
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+import re
+import datetime
+import numpy as np
+
+from PIL import Image
+import sys
+import pyocr.builders
+import image_processing
+
+THRESHOLD = 0.95
+TEMPLATE = cv2.imread('template.jpg')
+TEMPLATE2 = cv2.imread('time.jpg')
+
+
+class ExerciseData:
+    def __init__(self, time, cal,distance):
+        self.time = time
+        self.cal = cal
+        self.distance = distance
+
+@unique
+class ImageType(Enum):
+    EXERCISE_IMAGE = auto()
+    CUSTOM_EXERCISE_IMAGE = auto()
+
+def read_cal_by_tesseract(image):
+    image = image_processing.skew_image(image)
+
+    # 大津の二値化をしておく
+    im_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, image = cv2.threshold(im_gray, 0, 255, cv2.THRESH_OTSU)
+
+    image = Image.fromarray(image)
+
+    tools = pyocr.get_available_tools()
+    if len(tools) == 0:
+        print("No OCR tool found")
+        sys.exit(1)
+    # The tools are returned in the recommended order of usage
+    tool = tools[0]
+    langs = tool.get_available_languages()
+    print("Available languages: %s" % ", ".join(langs))
+    lang = langs[0]
+    print("Will use lang '%s'" % (langs))
+
+    txt = tool.image_to_string(
+        image,
+        lang=lang,
+        builder=pyocr.builders.TextBuilder(tesseract_layout=6)
+    )
+    print(txt)
+
+    # よくある誤字を修正
+    retext = txt.replace(' ', '').replace('i', '1').replace(']', '1').replace('t', '1') \
+        .replace('?', '2').replace('O', '0').replace('A', '4').replace('l', '1').replace('I', '1') \
+        .replace('Q', '9').replace('g', '9').replace('/', '7')
+
+    # 数字以外はすべて取り除き、intにする
+    try:
+        retext = int(re.sub('[^0-9]', '', retext))
+    except ValueError as e:
+        print("this is empty!")
+        retext = 0
+
+    # kcalが4桁はあり得ないのでエラー
+    try:
+        if (retext > 999):
+            raise ValueError("Cal is too large!")
+    except ValueError as e:
+        print(e)
+        retext = int(re.sub('[^0-9]', '', txt))
+
+    if str(retext) != txt: print("Text is replaced.")
+    return int(retext)
+
+
+def is_result_image():
+    fetch_image = cv2.imread('temp.jpg')
+
+    # fetch_imageのsizeが公式のものでなければout
+    h, w, _ = fetch_image.shape
+    if h != 720 or w != 1280:
+        return None
+
+    result = cv2.matchTemplate(fetch_image, TEMPLATE, cv2.TM_CCOEFF_NORMED)
+    # 検出結果から検出領域の位置を取得
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    print(max_val)
+    if max_val > THRESHOLD:
+        return ImageType.EXERCISE_IMAGE
+    result = cv2.matchTemplate(fetch_image, TEMPLATE2, cv2.TM_CCOEFF_NORMED)
+    # 検出結果から検出領域の位置を取得
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    print(max_val)
+    if max_val > THRESHOLD:
+        return ImageType.CUSTOM_EXERCISE_IMAGE
+    else:
+        return None
+
+
+def image_to_data(imagetype):
+    fetch_image = cv2.imread('temp.jpg')
+
+    # 各部分の画像を切り出す
+    if imagetype == ImageType.EXERCISE_IMAGE:
+        # total_time = fetch_image[257:257 + 84, 552:552 + 404]
+        total_cal = fetch_image[396:396 + 65, 586:586 + 228]
+        # total_distance = fetch_image[493:493 + 76, 560:560 + 396]
+    elif imagetype == ImageType.CUSTOM_EXERCISE_IMAGE:
+        total_cal = fetch_image[437:437 + 65, 584:584+ 228]
+    else:
+        print(imagetype)
+        raise ValueError("An error has ocurred! Image is not a exercise image.")
+
+    # dummyData
+    time = datetime.time(second=0)
+    cal = read_cal_by_tesseract(total_cal)
+    distance = 0
+    return ExerciseData(time, cal,distance)
+
+
+def convert_datalist_to_timelist(exercise_data_list):
+    return [e.time.hour + (e.time.minute / 60.0) + (e.time.second / 3600.0) for e in
+            exercise_data_list]
+
+
+def convert_datatuple_to_list(exercise_data_list):
+    return [e[0] for e in exercise_data_list]
+
+
+def datalist_to_histogram(exercise_list, ranking):
+    plt.figure()
+    n, bins, patches = plt.hist(exercise_list, color='darkturquoise', ec='black', bins=30)
+    print(n, bins)
+    print(exercise_list[ranking])
+    print("xは", np.where(exercise_list[ranking] >= bins)[0][-1])
+    x = np.where(exercise_list[ranking] >= bins)[0][-1]
+    if x == 30: x = x - 1
+    patches[x].set_facecolor('tomato')
+    # y軸を整数にする
+    plt.gca().get_yaxis().set_major_locator(ticker.MaxNLocator(integer=True))
+    plt.savefig('hist.png')  # ヒストグラムを保存
+    plt.close()
+
+
+if __name__ == '__main__':
+    import sqlite3
+    import config
+
+    # ヒストグラムのテスト
+    conn = sqlite3.connect(config.DATABASE_NAME)
+    cur = conn.cursor()
+    cur.execute("select kcal from Exercise "
+                "WHERE date(time_stamp) == date('now', '+9 hours') and kcal < 150 ORDER BY kcal DESC ;")
+    exercise_data_list = cur.fetchall()
+    print(exercise_data_list)
+    # num_list = convert_datatuple_to_list(exercise_data_list)
+
+    num_list = [0, 10, 10, 10, 10.1, 30, 30, 30, 30.2, 40, 50, 70, 90, 200]
+    time_ranking = 10
+    plt.figure()
+    n, bins, patches = plt.hist(num_list, color='darkturquoise', ec='black', bins=30)
+    print(n, bins)
+    print(np.where(num_list[time_ranking] >= bins)[0])
+
+    print(np.where(num_list[time_ranking] >= bins)[0][-1])
+    x = np.where(num_list[time_ranking] >= bins)[0][-1]
+    if x == 20: x = 19
+    patches[x].set_facecolor('tomato')
+    plt.gca().get_yaxis().set_major_locator(ticker.MaxNLocator(integer=True))
+    plt.savefig('hist.png')
+    plt.show()
+
+    fetch_image = cv2.imread('images/nonread.jpg')
+    result = cv2.matchTemplate(fetch_image, TEMPLATE, cv2.TM_CCOEFF_NORMED)
+    # 検出結果から検出領域の位置を取得
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    print(max_val)
+    if max_val > THRESHOLD:
+        print(ImageType.EXERCISE_IMAGE)
+    result = cv2.matchTemplate(fetch_image, TEMPLATE2, cv2.TM_CCOEFF_NORMED)
+    # 検出結果から検出領域の位置を取得
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    print(max_val)
+
+    # 各部分の画像を切り出す
+    total_cal = fetch_image[396:396 + 65, 586:586 + 228]
+
+    # time = read_time(total_time)
+    # dummyData
+    time = datetime.time(second=0)
+    cal = read_cal_by_tesseract(total_cal)
---
+++ tweet_func.py
@@ -0,0 +1,226 @@
+# -*- coding: utf-8 -*-
+import tweepy
+import datetime
+import config
+import urllib
+import info_convert
+import MySQLdb
+import traceback
+import utils
+from PIL import Image, ImageDraw, ImageFont
+
+
+# TwitterのAPI_TOKEN
+CK = config.CONSUMER_KEY
+CS = config.CONSUMER_SECRET
+AT = config.ACCESS_TOKEN
+AS = config.ACCESS_TOKEN_SECRET
+
+TWITTER_ID = config.TWITTER_ID
+
+# タイムゾーン指定
+JST = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
+
+conn = MySQLdb.connect(
+    host='localhost', user=config.DATABASE_USER, passwd=config.DATABASE_PASS, db=config.DATABASE_NAME,
+    charset='utf8mb4')
+
+
+# TwitterAPI認証用関数
+def auth_twitter():
+    auth = tweepy.OAuthHandler(CK, CS)
+    auth.set_access_token(AT, AS)
+    api = tweepy.API(auth, wait_on_rate_limit=True)  # API利用制限にかかった場合、解除まで待機する
+    return api
+
+
+# 運動記録をツイッター上から検索し、データベースに追加する, フォローしてくれている人にはリプライする。
+def search_exercise_data(api, max_number=300,interrupt=True):
+    cur = conn.cursor()
+    # フォローしてくれている人を取得
+    follower_id = api.followers_ids()
+
+    for tweet in tweepy.Cursor(api.search, q=f'#リングフィットアドベンチャー -filter:retweets filter:images',tweet_mode="extended").items(max_number):
+        print(tweet.full_text)
+        # idが重複していたら、すでにそこまで検索してあるので中断
+        cur.execute("select count(*) from Exercise where tweet_id = %s", (tweet.id,))
+        if int(cur.fetchone()[0]) :
+            if interrupt: return
+            else: continue
+        # imgがリングフィットのものでなければcontinue
+        image_type = fetch_image(tweet)
+        if image_type is None: continue
+        try:
+            # 画像から運動記録を読み取る
+            exercise_data = info_convert.image_to_data(image_type)
+            print(exercise_data.cal)
+            # DBに運動記録を追加
+            params = (exercise_data.cal,tweet.user.name,tweet.user.screen_name,tweet.id,tweet.created_at+ datetime.timedelta(hours=9))
+            cur.execute(
+                "insert into Exercise (kcal,user_name,user_screen_name,tweet_id,tweeted_time) "
+                "values (%s,%s,%s,%s,%s) ",params
+            )
+            conn.commit()
+
+            # もしフォローしてくれている人なら、順位を呟く
+            if tweet.user.id in follower_id:
+                print(tweet.user.screen_name," さんにお返事します")
+                reply_exercise_result(api,cur,exercise_data,tweet)
+
+        except tweepy.error.TweepError:
+            traceback.print_exc()
+            utils.send_mail("Tweepy has occurred.", traceback.format_exc())
+
+
+# @{TWITTER_ID}へのリプに対し、順位を返信する。
+# TODO:開発中/まだ使えません
+def reply_ranking(api,item_num=100):
+    for tweet in tweepy.Cursor(api.search, q=f'@{TWITTER_ID}',tweet_mode="extended").items(item_num):
+        print(tweet.full_text)
+        # ツイートに順位 が含まれているなら、順位をリプライする
+        if not "順位" in tweet.full_text:
+            return
+        cur = conn.cursor()
+
+        now = datetime.datetime.now(JST)
+        ranking_timestamp = now - datetime.timedelta(hours=4)
+
+        params = (tweet.user.id,ranking_timestamp)
+
+        # user_idからカロリーを抽出
+        cur.execute("SELECT kcal FROM Exercise WHERE user_id == %s AND  CAST(tweeted_time - INTERVAL 4 HOUR AS DATE)=CAST(%s AS DATE)",params)
+
+
+def make_ranking_picture(exercise_data_list):
+    # ベース画像を読み込む
+    im = Image.open('ranking_template.png')
+    draw = ImageDraw.Draw(im)
+
+    # 枠幅
+    W_song = 390
+
+    # ユーザ名のフォントサイズとフォント種類
+    font_size_ranking = 35
+    font_ranking = ImageFont.truetype(config.RANKING_FONT,font_size_ranking)
+    # 消費カロリーのフォントサイズとフォント種類
+    font_size_point = 25
+    font_point = ImageFont.truetype(config.KCAL_FONT,font_size_point)
+
+    # 標準のフォントカラー
+    font_color = (200, 0, 0)
+    # 消費カロリー用のフォントカラー
+    font_color_point = (150, 0, 0)
+
+    # ランキング
+    for i, exercise_data in enumerate(exercise_data_list):
+        user_name = exercise_data[0]
+        kcal = f"{exercise_data[1]}kcal"
+
+        # 1位から5位、6位から10位でポジションを変更
+        w_pos_s = 15 if i < 5 else 610
+        w_pos_p = 0 if i < 5 else 605
+        h_pos = i if i < 5 else i - 5
+
+        # 消費カロリーを描画
+        draw.font = font_point
+        draw.text((490 + w_pos_p, 170 + 99 * h_pos),
+                  kcal, font_color_point)
+
+        # ユーザ名を描画
+        draw.font = font_ranking
+        w_song, h_song = draw.textsize(user_name)
+        # 枠をはみ出す場合は縮小する。
+        if w_song > W_song:
+            font_size = int(font_size_ranking * W_song / w_song)
+            draw.font = ImageFont.truetype(config.RANKING_FONT,font_size)
+            w_song_n, h_song_n = draw.textsize(user_name)
+            draw.text(
+                (85 + w_pos_s, 165 + 97 * h_pos +
+                 (h_song - h_song_n) * h_song_n / h_song * 1.1),
+                user_name, font_color)
+        else:
+            draw.text((85 + w_pos_s, 165 + 97 * h_pos), user_name, font_color)
+
+    im.save("ranking_picture.png")
+
+# 運動記録のランキングをツイートする
+def tweet_ranking(api):
+    cur = conn.cursor()
+    # DBから前日分の運動結果を抽出し、消費カロリーの多い順でソート
+    # 昨日の04:00:00 から 今日の03:59:59まで
+    now = datetime.datetime.now(JST)
+    yesterday = now - datetime.timedelta(days=1)
+    yesterday = yesterday.strftime("%Y-%m-%d")
+    params = (yesterday, )
+
+    cur.execute("SELECT user_name,kcal "
+                "FROM (SELECT *, RANK() OVER(PARTITION BY user_screen_name ORDER BY kcal DESC, tweeted_time ASC) AS rnk FROM Exercise WHERE CAST(tweeted_time - INTERVAL 4 HOUR AS DATE)=CAST(%s AS DATE)) tmp "
+                "WHERE rnk = 1 ORDER BY kcal DESC, tweeted_time ASC  LIMIT 10;",params)
+
+
+    exercise_data_list = cur.fetchall()
+    make_ranking_picture(exercise_data_list)
+    tweet = "今日のランキング発表！\n"
+    for i, exercise_data in enumerate(exercise_data_list):
+        tweet += f"{i + 1}位 {exercise_data[0]} {exercise_data[1]}kcal\n"
+        if i + 1 >= 3: break
+    print(tweet)
+    api.update_with_media(status=tweet, filename='./ranking_picture.png')
+
+
+# 運動結果の画像を取得出来たらimagetype,できなかったらNoneを返す
+def fetch_image(status):
+    # mediaがなければ飛ばす
+    if not hasattr(status, 'extended_entities'):
+        print("Media not found.")
+        return None
+    for i in range(len(status.extended_entities['media'])):
+        # mediaを保存する
+        media_url = status.extended_entities['media'][i]['media_url']
+        try:
+            urllib.request.urlretrieve(media_url + ':orig', 'temp.jpg')
+        except IOError:
+            # 保存に失敗したら飛ばす
+            print("save miss")
+            continue
+        # 運動結果の画像でなければ飛ばす
+        image_type = info_convert.is_result_image()
+        if image_type is None:
+            print("Media is not exercise image.")
+            continue
+        else:
+            return image_type
+    return None
+
+def reply_exercise_result(api,cur,exercise_data,status):
+
+    now = datetime.datetime.now(JST)
+    ranking_datetime = now - datetime.timedelta(hours=4)
+    ranking_datetime = ranking_datetime.strftime("%Y-%m-%d")
+    params=(ranking_datetime,)
+
+    # DBから今日の順位分のデータを抽出し、消費カロリー順でソート
+
+    cur.execute("select kcal from Exercise "
+                "WHERE  CAST(tweeted_time - INTERVAL 4 HOUR AS DATE)=CAST(%s AS DATE) ORDER BY kcal DESC ;",params)
+    exercise_data_list = cur.fetchall()
+    print(exercise_data)
+
+    # 消費カロリーの順位を計算する
+    params = (exercise_data.cal,ranking_datetime)
+    cur.execute("select count(*) from Exercise WHERE Exercise.kcal > %s "
+                "AND  CAST(tweeted_time - INTERVAL 4 HOUR AS DATE)=CAST(%s AS DATE)", params)
+    cal_ranking = int(cur.fetchone()[0])
+    print(cal_ranking)
+
+    tweet = "@" + str(status.user.screen_name) + '\n'
+    tweet += str(exercise_data.cal) + "kcal消費 いい汗かいたね！お疲れ様！\n"
+    tweet += f"今日の順位 {cal_ranking + 1}位/{len(exercise_data_list)}人中"
+    print(exercise_data_list)
+    info_convert.datalist_to_histogram(info_convert.convert_datatuple_to_list(exercise_data_list),
+                                       cal_ranking)
+    api.update_with_media(status=tweet, in_reply_to_status_id=status.id, filename='./hist.png')
+
+if __name__ == '__main__':
+    api = auth_twitter()
+    tweet_ranking(api)
